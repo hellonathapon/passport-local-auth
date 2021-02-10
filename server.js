b1a610249ee93fc182e6d0209db0386fecf76d08
hellonathapon <hellonathapon@gmail.com>
2021-02-10 08:38:12 +0700

    add session

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 // db connection
@@ -11,6 +14,22 @@ mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true, useUnifiedTopol
 
 // ejs
 app.set('view engine', 'ejs');
+
+// global middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// setup session
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: {
+        maxAge: 1000 * 60 * 60 *24, // 1 day
+    }
+}))
 
 app.use('/', require('./routes/index.js'));
 
