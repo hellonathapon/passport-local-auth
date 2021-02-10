e95abaa428ae21591d3d6d191aa949164a2800d6
hellonathapon <hellonathapon@gmail.com>
2021-02-10 08:27:34 +0700

    add Database connection

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// db connection
+mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log(`database connected!`))
+.catch((err) => console.err(err));
 
 // ejs
 app.set('view engine', 'ejs');
