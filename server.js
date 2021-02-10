e4f5e1bf886df737644a0c05acf7ecba5fb48959
hellonathapon <hellonathapon@gmail.com>
2021-02-10 08:16:58 +0700

    add router and ejs

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// ejs
+app.set('view engine', 'ejs');
 
+app.use('/', require('./routes/index.js'));
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
